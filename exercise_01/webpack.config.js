7592a2bae6c08725f61cc2995949074318c0983e
yijaejun <yijaejun81@gmail.com>
2016-11-10 13:37:44 +0900

    no message

--- exercise_01/webpack.config.js
+++ exercise_01/webpack.config.js
@@ -5,7 +5,8 @@ var config = {
 		path: __dirname + "/public/",
 		filename: "bundle.js"
 	},
-
+	// TODO css 및 js를 압축하는 모듈을 추가할 것.
+	// TODO Requirejs or commonsjs를 통해서 AMD를 도입하여 필요한 라이브러리만 로딩하여 사용할 수 있도록 하여 클라이언트 부하분산처리를 할 수 있도록 한다
 	module: {
 		loaders: [{
 			test: /\.jsx?$/,
