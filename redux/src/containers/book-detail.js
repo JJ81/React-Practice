7feddaa79c72187445e9128e9a83261e7f63cf1c
yijaejun <yijaejun81@gmail.com>
2016-12-24 18:59:21 +0900

    add commnet

--- redux/src/containers/book-detail.js
+++ redux/src/containers/book-detail.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
+// 데이터를 받아서 갱신하여 보여주기만 하기 때문에 action이 바인딩되지 않는다. bindActionCreators가 필요 없다.
+// 반면, book-list의 경우 필요하며, dispatch가 사용되어 connect에 연결이 된다.
 class BookDetail extends Component{
   render() {
     if(!this.props.book){
